056f3f4a8f4fc14b2a4c75cff3819421426922c4
lohana2905 <93660761+lohana2905@users.noreply.github.com>
2021-11-03 10:49:52 -0300

    Update principal.js

--- js/principal.js
+++ js/principal.js
@@ -1,63 +1,60 @@
-    var botaoAdicionar = document.querySelector("#adicionar-paciente")
-        botaoAdicionar.addEventListener("click", function(){
+//Lohana de Aguiar Bristoti 
+/*IFPR Campus Cascavel 
+Programação para Internet 
+Professor Fernando 
+03-11-2021*/    
+var botaoAdicionar = document.querySelector("#adicionar-paciente") // cria o botão 'Adicionar' atravéz do método
+        botaoAdicionar.addEventListener("click", function()
+        {
             event.preventDefault()    
             var form = document.querySelector("#form-adiciona")      
 
             var paciente = obtemPacienteDoFormulario(form)
-
-            var erros = validaPaciente(paciente)
-            console.log(erros)
-
-            if (erros.length > 0){
-                exibeMensagensDeErro(erros)
-                return
-            }
-
             var pacienteTr = montaTr(paciente)
             
             var tabela = document.querySelector("#tabela-pacientes");
 
             tabela.appendChild(pacienteTr);
-            form.reset()
-
-            var mensagensErro = document.querySelector("#mensagens-erro")
-            mensagensErro.innerHTML = ""
+            //botão adicionar reune os dados do paciente e coloca mais uma linha na tabela
         })
         
-    function obtemPacienteDoFormulario(form) {
-        var paciente = {
-             nome : form.nome.value,
-             peso : form.peso.value,
-             altura : form.altura.value,
-             gordura : form.gordura.value,
-             imc: calculaImc(form.peso.value, form.altura.value)
+    function obtemPacienteDoFormulario(form) //Cria uma função que recebe as informações do paciente por meio de um formulário
+    {
+        var paciente = //cria o método paciente
+        {
+             nome : form.nome.value, //adiciona o nome do paciente no formulário
+             peso : form.peso.value, //adiciona o peso do paciente no formulário
+             altura : form.altura.value, //adiciona a altura do paciente no formulário
+             gordura : form.gordura.value, //adiciona a gordura do paciente no formulário
+             imc: calculaImc(form.peso.value, form.altura.value) //adiciona o IMC do paciente no formulário por meio da função 'calculaImc'
         }
-        return paciente
+        return paciente //retorna os valores que foram determinados nas variáveis da função 'obtemPacienteDoFormulário'
     }
 
-    function montaTr(paciente){
-        var pacienteTr = document.createElement("tr");
+    function montaTr(paciente) //função para montar uma linha na tabela com as informações do paciente
+    {
+        var pacienteTr = document.createElement("tr"); //cria o método
 
-        pacienteTr.classList.add("paciente")
+        pacienteTr.classList.add("paciente") //chama as informações que estão guardadas no método 'paciente'
 
-        pacienteTr.appendChild(montaTd(paciente.nome, "info-nome"))
-        pacienteTr.appendChild(montaTd(paciente.peso, "info-peso"))
-        pacienteTr.appendChild(montaTd(paciente.altura, "info-altura"))
-        pacienteTr.appendChild(montaTd(paciente.gordura, "info-gordura"))
-        pacienteTr.appendChild(montaTd(paciente.imc, "info-imc"))
+        pacienteTr.appendChild(montaTd(paciente.nome, "info-nome")) //recebe na tabela o dado do método 'paciente' que contém o nome do paciente
+        pacienteTr.appendChild(montaTd(paciente.peso, "info-peso")) //recebe na tabela o dado do método 'paciente' que contém o peso do paciente
+        pacienteTr.appendChild(montaTd(paciente.altura, "info-altura")) //recebe na tabela o dado do método 'paciente' que contém a altura do paciente
+        pacienteTr.appendChild(montaTd(paciente.gordura, "info-gordura")) //recebe na tabela o dado do método 'paciente' que contém índice de gordura do paciente
+        pacienteTr.appendChild(montaTd(paciente.imc, "info-imc")) //recebe na tabela o dado do método 'paciente' que contém o IMC do paciente
 
-        return pacienteTr
+        return pacienteTr //retorna uma linha da tabela 
     }
 
-    function montaTd(dado, classe) {
+    function montaTd(dado, classe) //cria a tabela com o nº de linhas igual a quantidade de pacienteTr que têm
+    {
         var td = document.createElement("td")
         td.classList.add(classe)
         td.textContent = dado
 
-        return td
+        return td //retorna a tabela 
     }
-
-    function validaPaciente(paciente){
+    function validaPaciente(paciente){ //função para verificar a validação do paciente
 
         var erros = []
 
@@ -88,7 +85,7 @@
         return erros
     }
 
-    function exibeMensagensDeErro(erros){
+    function exibeMensagensDeErro(erros){ // chama a mensagem de erro de acordo com tipo de erro que ocorrer
         var ul = document.querySelector("#mensagens-erro")
         ul.innerHTML = ""
 
