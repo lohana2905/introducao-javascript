bff2edbc4566189ab262a99e718c24d5769f04dc
lohana2905 <93660761+lohana2905@users.noreply.github.com>
2021-11-08 11:25:34 -0300

    Update remover-paciente.js

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,11 +1,11 @@
-var pacientes = document.querySelectorAll(".paciente")
+var pacientes = document.querySelectorAll(".paciente") // variavel recebe pacientes
 
-var tabela = document.querySelector("#tabela-pacientes")
+var tabela = document.querySelector("#tabela-pacientes") // variavel recebe tabela de pacientes 
 
 tabela.addEventListener("dblclick", function(event){
-    event.target.parentNode.classList.add("fadeOut")
+    event.target.parentNode.classList.add("fadeOut") //fadeOut é a transição da remoção
 
-    setTimeout(function(){
-        event.target.parentNode.remove()
+    setTimeout(function(){ //Pede para esperar x tempo até executar o comando seguinte.
+        event.target.parentNode.remove() //remove exatamente o local clicado
     }, 500)
 })
